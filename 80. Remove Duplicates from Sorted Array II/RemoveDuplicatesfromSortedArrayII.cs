6c5a1577bf9d6bb0fc72454aab881be6b361b98f
Sushant Kumar <63387227+sush-2012@users.noreply.github.com>
2024-03-01 12:23:59 +0530

    Add files via upload

---
+++ 80. Remove Duplicates from Sorted Array II/RemoveDuplicatesfromSortedArrayII.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int i=0;
+        foreach(int num in nums){
+            if(i < 2 || num > nums[i-2]){
+                nums[i++] = num;
+            }
+        } 
+        return i;
+    }
+}
